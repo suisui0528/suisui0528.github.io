00e8d1bd49efdb5006e5a9b1fbd4d3b96c8db78d
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-03-01 15:43:40 +0800

    feat(assets): 新增網頁圖示
    - 將 favicon.ico 加入資源資料夾
    - 於 <head> 新增 link:icon 標籤

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import favicon from '$lib/assets/favicon.ico';
 	import './layout.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} type="image/x-icon" />
+</svelte:head>
 
 {@render children()}
