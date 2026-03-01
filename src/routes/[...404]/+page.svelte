5a708b907bc12bed8d7e568a5d96cd35025729d2
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-03-01 21:10:06 +0800

    feat(404-page): 新增 404 頁面

---
+++ src/routes/[...404]/+page.svelte
@@ -0,0 +1,6 @@
+<div class="my-20 flex flex-col gap-8">
+	<span class="text-center text-6xl font-bold">404 Not Found</span>
+	<button class="mx-auto w-fit rounded-lg bg-sys-accent px-6 py-3 text-2xl text-white">
+		<a href="/">返回</a>
+	</button>
+</div>
